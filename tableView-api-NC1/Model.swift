935e3701a99f2cd50c19be23c8d5db1d65ca3fad
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-01-25 20:36:49 +0330

    cV-add API

---
+++ tableView-api-NC1/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  tableView-api-NC1
+//
+//  Created by Ali esmaeili on 1/25/23.
+//
+
+import Foundation
+
+struct ToDo: Decodable {
+    
+    let localized_name : String
+    let img : String
+}
--- tableView-api-NC1/ViewController.swift
+++ tableView-api-NC1/ViewController.swift
@@ -3,12 +3,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    var data = [ToDo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        APIImages(URL: "https://api.opendota.com/api/heroStats") { result in print(result)}
     }
+    
+    
+    func APIImages(URL url:String, completion: @escaping ([ToDo]) -> Void) {
+        
+        let url = URL(string: url)
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url!) { data,response,error in
+                do {
+                    let fetchingData = try JSONDecoder().decode([ToDo].self, from:data!)
+                    completion(fetchingData)
+                } catch {
+                    
+                    print("Parsing Error")
+                }
+            }
+        
+        dataTask.resume()
 
-
-}
+        }
+        
+    }
 
