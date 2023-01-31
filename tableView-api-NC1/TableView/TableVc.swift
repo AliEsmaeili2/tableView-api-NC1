b1f71e16752a266d77226daba07156b44951f3e9
Ali Esmaeili <ali.esmaeili1927@gmail.com>
2023-01-31 16:00:05 +0330

    update buttonCell

--- tableView-api-NC1/TableView/TableVc.swift
+++ tableView-api-NC1/TableView/TableVc.swift
@@ -6,7 +6,12 @@ class TableVc: UIViewController ,UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     //connect tableView in main.storyboard to ViewController
     
+   @IBOutlet weak var searchBar: UISearchBar!
+    
     var heroes = [HeroStats]()
+    
+   // var searchingNames = [String]()
+  //  var searching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +28,15 @@ class TableVc: UIViewController ,UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return heroes.count
+        
+  //      if searching {
+            
+ //           return searchingNames.count
+            
+  //      } else {
+            
+            return heroes.count
+ //       }
     }
     
     // for CellRow ->take(img & HeroName)in TableView From JSON(API) for Load to CustomTableViewCell
@@ -31,21 +44,40 @@ class TableVc: UIViewController ,UITableViewDelegate, UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
         
+        
         let apiData : HeroStats
         apiData = heroes[indexPath.row]
         
+        //for load img from JSON
         let string = "https://api.opendota.com" + (apiData.img)
         let url = URL(string: string)
         
         cell.imageCell.downloadedFrom(url: url!, contentMode: .scaleToFill)
         cell.nameCell.text = apiData.localized_name
         
+        /*
+       if searching {
+            
+            cell.nameCell.text = searchingNames[indexPath.row]
+
+            
+        } else {
+            
+            cell.nameCell.text = heroes[indexPath.row].localized_name
+            
+        }
+         */
+    
+        // for Button
         cell.cellBtn = {[unowned self] in
             
             let listName = self.heroes[indexPath.row].localized_name
             let listAtt = self.heroes[indexPath.row].primary_attr
+            let listAttack_type = self.heroes[indexPath.row].attack_type
 
-            let alert = UIAlertController(title: "\(listName)", message: "Primary_Attribute: \(listAtt)", preferredStyle: .alert)
+            
+            
+            let alert = UIAlertController(title: "\(listName)", message: "Primary_Attribute: \(listAtt) \n Attack_Type: \(listAttack_type)", preferredStyle: .alert)
             
             let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
             
@@ -57,6 +89,22 @@ class TableVc: UIViewController ,UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+        /*
+        
+        func searchBar(_ searchBar : UISearchBar, textDidChange searchText: String){
+            
+            searchingNames = heroes.filter({$0.prefix(searchText.count) == searchText})
+            
+             searching = true
+            tableView.reloadData()
+        }
+    
+    func searchBarCancelButtonClicked(_ searchBar : UISearchBar) {
+        
+        searching = false
+        searchBar.text = ""
+    }
+*/
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         performSegue(withIdentifier: "showDetails", sender: self)
